6c82c2345a08f0f99cbabea63fb9692a2bebdb0d
thomasrichardson439 <richardsonthomas439@gmail.com>
2020-06-09 19:10:01 +0300

    Changed font family

--- components/AccordionItem.vue
+++ components/AccordionItem.vue
@@ -1,30 +1,35 @@
 <template>
-  <div>
-    <div class="flex justify-between p-4">
-      <p class="text-lg font-semi-bold font-black">{{question}}</p>
-      <img
-        class="w-6 h-4 mx-2 float-right self-center"
-        src="~/assets/images/pricing/chevron.svg"
-        :class="{ 'transform': open, 'rotate-180': open}"
-        @click="toggle"
-      />
+    <div>
+        <div class="flex justify-between p-4">
+            <p class="text-lg font-semi-bold font-black">{{ question }}</p>
+            <img
+                class="w-6 h-4 mx-2 float-right self-center"
+                src="~/assets/images/pricing/chevron.svg"
+                :class="{ transform: open, 'rotate-180': open }"
+                @click="toggle"
+            />
+        </div>
+        <p
+            class="accordion-text mb-4 px-4 font-normal text-gray-500"
+            v-if="open"
+        >
+            {{ answer }}
+        </p>
     </div>
-    <p class="accordion-text mb-4 px-4 font-normal text-gray-500" v-if="open">{{answer}}</p>
-  </div>
 </template>
 
 <script>
 export default {
-  props: ['question', 'answer'],
-  data() {
-    return {
-      open: false
+    props: ['question', 'answer'],
+    data() {
+        return {
+            open: false
+        }
+    },
+    methods: {
+        toggle() {
+            this.open = !this.open
+        }
     }
-  },
-  methods: {
-    toggle() {
-      this.open = !this.open
-    }
-  }
 }
 </script>
