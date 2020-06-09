95be7f734437301d82d4e3f4a556475b7a76aca0
thomasrichardson439 <richardsonthomas439@gmail.com>
2020-06-09 14:52:43 +0300

    Pricing page

---
+++ components/AccordionItem.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div class="flex justify-between">
+      <p class="text-lg font-semi-bold font-black">{{title}}</p>
+      <img
+        class="w-4 h-3 mx-2 float-right self-center"
+        src="~/assets/images/pricing/arrow-down.png"
+        :class="{ 'transform': open, 'rotate-180': open}"
+        @click="toggle"
+      />
+    </div>
+    <p class="accordion-text" v-if="open">{{text}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['title', 'text'],
+  data() {
+    return {
+      open: false
+    }
+  },
+  methods: {
+    toggle() {
+      this.open = !this.open
+    }
+  }
+}
+</script>
